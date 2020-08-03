c789900469e68bee6fec53548697fd4f40e1cfc6
cfuntila <cfuntila@ucsc.edu>
2020-08-02 17:01:01 -0700

    added unit tests for unexpected input

--- test_journal.py
+++ test_journal.py
@@ -11,20 +11,68 @@ class TestJournal(unittest.TestCase):
         os.remove("data.json")
 
     def test_expected_create_1(self):
-        # print('in test 1')
-        os.system("./journal.py --title \"ttt\" --create \"ccccc\"")
+        title = "ttt"
+        content = "ccc"
+        os.system(f"./journal.py --title {title} --create {content}")
+
         with open("data.json") as f:
             currData = json.load(f)
         
-        self.assertTrue(currData[-1]["title"] == "ttt" )
+        self.assertTrue(currData[-1]["title"] == title)
+        self.assertTrue(currData[-1]["content"] == content)
     
     def test_expected_create_2(self):
-        # print('in test 2')
-        os.system("./journal.py --create \"ccccc\" --title \"newttt\"")
+        title = "newTTT"
+        content = "newCCC"
+        os.system(f"./journal.py --create {content} --title {title}")
+
         with open("data.json") as f:
             currData = json.load(f)
-        self.assertTrue(currData[-1]["title"] == "newttt" )
-   
+
+        self.assertTrue(currData[-1]["title"] == title)
+        self.assertTrue(currData[-1]["content"] == content)
+    
+    def test_unexpected_title_no_content(self):
+        title = "addedOnlyTitle"
+        os.system(f"./journal.py --title {title}")
+
+        with open("data.json") as f:
+            currData = json.load(f)
+
+        self.assertTrue(currData[-1]["title"] != title)
+    
+    def test_unexpected_content_no_title(self):
+        
+        content = "addedOnlyContent"
+        os.system(f"./journal.py --create {content}")
+
+        with open("data.json") as f:
+            currData = json.load(f)
+
+        self.assertTrue(currData[-1]["content"] != content)
+    
+    def test_unexpected_empty_content_empty_title(self):
+        os.system("./journal.py --create \" \" --title \" \" ")
+
+        with open("data.json") as f:
+            currData = json.load(f)
+        
+        self.assertTrue(currData[-1]["title"] != " ")
+        self.assertTrue(currData[-1]["content"] != " ")
+    
+    def test_unexpected_multiple_entries_same_title(self):
+        title = "sameTitle"
+        content1 = "content1"
+        content2 = "content2"
+
+        os.system(f"./journal.py --title {title} --create {content1}")
+        os.system(f"./journal.py --title {title} --create {content2}")
+
+        with open("data.json") as f:
+            currData = json.load(f)
+
+        self.assertTrue(currData[-1]["title"] == title)
+        self.assertTrue(currData[-2]["title"] != title)
 
 if __name__ == '__main__':
     unittest.main()
